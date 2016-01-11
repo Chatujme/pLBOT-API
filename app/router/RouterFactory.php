808b3d8409c6f5be480803444fa770ed0b5d771f
LuRy <lury@lury.cz>
2016-01-11 23:54:15 +0100

    initial commit

---
+++ app/router/RouterFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Nette;
+use Nette\Application\Routers\RouteList;
+use Nette\Application\Routers\Route;
+
+
+class RouterFactory
+{
+
+	/**
+	 * @return Nette\Application\IRouter
+	 */
+	public static function createRouter()
+	{
+		$router = new RouteList;
+		$router[] = new Route('', 'Homepage:default');
+		$router[] = new Route('<action>[/<id>]', 'Api:default');
+		return $router;
+	}
+
+}
