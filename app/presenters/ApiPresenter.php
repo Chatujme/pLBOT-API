58cdb73e7cd250af9c9a7146491e7b1f816e140b
LuRy <lury@lury.cz>
2016-01-11 23:54:15 +0100

    initial commit

---
+++ app/presenters/ApiPresenter.php
@@ -0,0 +1,16 @@
+<?php
+namespace App;
+/**
+ * Description of ApiPresenter
+ *
+ * @author LuRy <lury@lury.cz>, <lukyrys@gmail.com>
+ */
+class ApiPresenter extends BasePresenter{
+    
+    protected function startup() {
+        parent::startup();
+    }
+    
+    
+    
+}
---
+++ app/presenters/BasePresenter.php
@@ -0,0 +1,15 @@
+<?php
+namespace App;
+/**
+ * Description of BasePresenter
+ *
+ * @author LuRy <lury@lury.cz>, <lukyrys@gmail.com>
+ */
+use Nette\Application\UI\Presenter;
+
+class BasePresenter extends Presenter{
+    
+    protected function startup() {
+        parent::startup();
+    }
+}
